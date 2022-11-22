1daecf3ca2aabca1040f6990f2298ebfda2d03de
jkocman <114053190+jkocman@users.noreply.github.com>
2022-11-22 16:28:43 +0100

    Add files via upload

---
+++ beep test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace beep_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] morseovka = new string[1,7];
+
+
+                for (int i = 0; i < morseovka.GetLength(0); i++)
+                {
+                    for (int j = 0; j < morseovka.GetLength(1); j++)
+                    {
+                        Console.Write($"{ morseovka[i, j],+10}");
+                    }
+                    Console.WriteLine();
+                }
+                Console.Write(@"===============================
+ 1 >> konverze z abecedy do morseovky
+ 2>> vypni/zapni Beep
+ 3>>
+ K >> ======konec
+=========================
+Tvůj výběr: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "k" || choice == "K")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
